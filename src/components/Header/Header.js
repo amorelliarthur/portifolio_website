28e5e690079395ce394b5ad362ed6fa3a052a51f
Arthur Amorelli <arthur-soares@hotmail.com>
2023-03-14 18:08:23 -0300

    Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,46 +3,48 @@ import React from 'react';
 import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
 import { DiCssdeck } from 'react-icons/di';
 
-import { Container, Div1, Div2, Div3, NavLink, SocialIcons } from './HeaderStyles';
+import { Container, Div1, Div2, Div3, NavLink, SocialIcons, Span } from './HeaderStyles';
 
 const Header = () =>  (
   <Container>
     <Div1>
       <Link legacyBehavior href="/">
-        <a style={{ display: 'flex', alignItems: 'center', color:"white" }}>
-          <DiCssdeck size="3rem" /> <span>Portfolio</span>
+        <a style={{ display: 'flex', alignItems: 'center', color:"white", marginBottom: "20px"}}>
+          <DiCssdeck size="3rem" /> <Span>Portfolio</Span>
         </a>
       </Link>
     </Div1>
+    
     <Div2>
       <li>
-        <Link href="#projects">
+        <Link legacyBehavior href="#projects">
           <NavLink>Projects</NavLink>
         </Link>
       </li>
       <li>
-        <Link href="#tech">
+        <Link legacyBehavior href="#tech">
           <NavLink>Technologies</NavLink>
         </Link>
       </li>        
       <li>
-        <Link href="#about">
+        <Link legacyBehavior href="#about">
           <NavLink>About</NavLink>
         </Link>
       </li>        
     </Div2>
-      <Div3>
-        <SocialIcons href="https://google.com">
-          <AiFillGithub size="3rem" />
-        </SocialIcons>
-        <SocialIcons href="https://google.com">
-          <AiFillLinkedin size="3rem" />
-        </SocialIcons>
-        <SocialIcons href="https://google.com">
-          <AiFillInstagram size="3rem"/>
-        </SocialIcons>
-      </Div3>
-    </Container>
+
+    <Div3>
+      <SocialIcons href="https://google.com">
+        <AiFillGithub size="3rem" />
+      </SocialIcons>
+      <SocialIcons href="https://google.com">
+        <AiFillLinkedin size="3rem" />
+      </SocialIcons>
+      <SocialIcons href="https://google.com">
+        <AiFillInstagram size="3rem"/>
+      </SocialIcons>
+    </Div3>
+  </Container>
 );
 
 export default Header;
