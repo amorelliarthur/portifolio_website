ae13e6c08838f9dbf526507e2383b9a82d120f17
Arthur Amorelli <arthur-soares@hotmail.com>
2023-03-21 11:06:05 -0300

    pt-br

--- next.config.js
+++ next.config.js
@@ -5,6 +5,13 @@ const nextConfig = {
 
 module.exports = nextConfig
 
+module.exports = {
+  i18n: {
+    locales: ['pt-BR'],
+    defaultLocale: 'pt-BR',
+  },
+}
+
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true'
 })
