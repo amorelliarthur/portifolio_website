ddd5868eba5619beac054fc8d616da3be4b8155f
Arthur Amorelli <arthur-soares@hotmail.com>
2023-03-27 18:00:01 -0300

    title

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -30,7 +30,7 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html lang='en-GB'>
-        <title>Portifólio - Arthur Amorelli</title>
+        <title>Portfólio - Arthur Amorelli</title>
         <Head>
           <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&display=swap" rel="stylesheet"/>
         </Head>
